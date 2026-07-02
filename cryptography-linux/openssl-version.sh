9fcaa6f40ddf730c3ce1e8462f22ec82271d734e
Seal <info@sealsecurity.io>
2026-07-02 13:43:24 +0300

    trigger macOS/Windows OpenSSL 3.5.7 builders

--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -5,3 +5,5 @@ export OPENSSL_SHA256="a8c0d28a529ca480f9f36cf5792e2cd21984552a3c8e4aa11a24aa31a
 export OPENSSL_BUILD_FLAGS_WINDOWS="no-zlib no-shared no-module no-comp no-apps no-docs no-sm2-precomp no-atexit"
 export OPENSSL_BUILD_FLAGS_ARMV7L="linux-armv4 ${OPENSSL_BUILD_FLAGS_WINDOWS}"
 export OPENSSL_BUILD_FLAGS="${OPENSSL_BUILD_FLAGS_WINDOWS} enable-ec_nistp_64_gcc_128"
+
+# seal: OpenSSL 3.5.7 pin for cryptography 46.0.5+sp1 (GHSA-537c-gmf6-5ccf)
